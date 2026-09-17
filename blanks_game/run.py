8bdf3479d226e5135c73dbf0f0fb6b9f83d0ab35
Calvin Courbois <97687907+esvalC@users.noreply.github.com>
2026-09-16 22:14:46 -0400

    Co-authored-by: aidanclark924 <aidanclark924@users.noreply.github.com>

--- blanks_game/run.py
+++ blanks_game/run.py
@@ -11,4 +11,4 @@ app = create_app()
 
 if __name__ == "__main__":
     host = "0.0.0.0" if "--lan" in sys.argv else "127.0.0.1"
-    app.run(host=host, port=5000, debug="--debug" in sys.argv)
+    app.run(host=host, port=5050, debug="--debug" in sys.argv)
